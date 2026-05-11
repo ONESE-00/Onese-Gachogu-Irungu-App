d78bb47c05d5597ddc9b6093d1ab80f73a2c71fb
Onese Gachogu <ogachogu@emtechhouse.co.ke>
2026-05-11 23:05:59 +0300

    setup

--- kcb-users/src/app/auth/login/login.component.ts
+++ kcb-users/src/app/auth/login/login.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../core/services/auth.service';
+import { WebSocket } from '../../core/services/web-socket/web-socket';
 
 @Component({
   selector: 'app-login',
@@ -13,11 +15,13 @@ export class LoginComponent implements OnInit {
   isLoading = false;
   errorMessage = '';
   showPassword = false;
+  private destroy$ = new Subject<void>()
 
   constructor(
     private fb: FormBuilder,
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private webSocketService: WebSocket,
   ) { }
 
   ngOnInit(): void {
@@ -29,6 +33,16 @@ export class LoginComponent implements OnInit {
     this.loginForm = this.fb.group({
       username: ['', [Validators.required, Validators.minLength(3)]],
       password: ['', [Validators.required, Validators.minLength(4)]],
+    });
+
+    this.webSocketService.connect('wss://echo.websocket.org');
+
+    this.webSocketService.getMessages()
+    .pipe(takeUntil(this.destroy$))
+    .subscribe({
+      next: (message) => this.errorMessage = `WebSocket Message: ${message}`,
+      error: (err) => console.error('WebSocket error:', err),
+      complete: () => console.log('WebSocket connection closed'),
     });
   }
 
@@ -45,17 +59,20 @@ export class LoginComponent implements OnInit {
     this.errorMessage = '';
 
     const { username, password } = this.loginForm.value;
-
-    this.authService.login(username, password).subscribe({
-      next: () => {
-        this.isLoading = false;
-        this.router.navigate(['/dashboard']);
-      },
-      error: (err) => {
-        this.isLoading = false;
-        this.errorMessage = err.message || 'Login failed. Please try again.';
-      },
-    });
+    this.webSocketService.send('Login successful');
+    this.isLoading = false;
+    // this.authService.login(username, password).subscribe({
+    //   next: () => {
+    //     this.isLoading = false;
+    //     // this.router.navigate(['/dashboard']);
+    //     this.webSocketService.send('Login successful');
+    //   },
+    //   error: (err) => {
+    //     this.isLoading = false;
+    //     this.webSocketService.send(err.message || 'Login failed');
+    //     // this.errorMessage = err.message || 'Login failed. Please try again.';
+    //   },
+    // });
   }
 
  
